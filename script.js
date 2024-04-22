2223002df22187d1f662d44dd4c80e868a00b23a
Rainers Bērziņš <berzins.rainers@gimnazija.local>
2024-04-22 14:55:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert(1);
+//console.log(1)
+let zina = document.querySelector(".zina");
+let zinas = document.querySelector(".catazina");
+
+function sutitZinu()
+{
+console.log("sutitZinu()darbojas")
+zinas.innerHTML = zinas.innerHTML+ "<br>"+zina.value;
+}
